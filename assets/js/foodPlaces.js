b61b0bfeb7b41eb23b8d410eb09beb8f48453e57
nithyalokuge <nithyasamadhilokuge@gmail.com>
2025-03-17 11:55:14 +0000

    Updated my page, homepage, facilities and contact ones. Improved general css, fixed style inconsistencies, removed unnecessary files

--- assets/js/foodPlaces.js
+++ assets/js/foodPlaces.js
@@ -1,11 +1,10 @@
-// Script for fetching data from foodPlaces.json, display it and make the Prev and Next navigation links work
+// Script for fetching data from foodPlaces.json and displaying it
 
 fetch('../assets/js/foodPlaces.json')
     .then(response => response.json())
     .then(data => {
         let foodPlaces = data;
         let filteredFoodPlaces = foodPlaces;
-        let currentPage = 0;
 
         // Function to display food places dynamically
         function displayFoodPlaces(places) {
@@ -16,7 +15,7 @@ fetch('../assets/js/foodPlaces.json')
                 const foodPlaceDiv = document.createElement('div');
                 foodPlaceDiv.classList.add('col');
                 foodPlaceDiv.innerHTML = `
-                    <div class="card">
+                    <div class="card whatsAround">
                         <img src="${place.image}" class="card-img-top" alt="${place.name} +'s + picture">
                         <div class="card-body">
                             <h5>${place.name}</h5>
@@ -30,14 +29,6 @@ fetch('../assets/js/foodPlaces.json')
             });
         }
 
-        function showPage(page) {
-            const placesPerPage = 3; // Show up to 3 cards per page
-            const start = page * placesPerPage;
-            const end = start + placesPerPage;
-            const pagePlaces = filteredFoodPlaces.slice(start, end);
-            displayFoodPlaces(pagePlaces);
-        }
-
         // Function to filter food places based on category
         function filterFoodPlaces(category) {
             if (category === 'all') {
@@ -45,8 +36,7 @@ fetch('../assets/js/foodPlaces.json')
             } else {
                 filteredFoodPlaces = foodPlaces.filter(place => place.category === category);
             }
-            currentPage = 0; 
-            showPage(currentPage);
+            displayFoodPlaces(filteredFoodPlaces);
         }
 
         // Event listener for filter buttons
@@ -65,30 +55,7 @@ fetch('../assets/js/foodPlaces.json')
             });
         });
 
-        // Event listeners for Prev and Next navigation links
-        document.getElementById('prev').addEventListener('click', () => {
-            if (currentPage > 0) {
-                currentPage--;
-            } else {
-                // Math.ceil object referenced from https://www.w3schools.com/jsref/jsref_ceil.asp, Accessed March 01, 2025
-                currentPage = Math.ceil(filteredFoodPlaces.length / 3) - 1;
-            }
-            showPage(currentPage);
-        });
-
-        document.getElementById('next').addEventListener('click', () => {
-            if (currentPage < Math.ceil(filteredFoodPlaces.length / 3) - 1) {
-                currentPage++;
-            } else {
-                currentPage = 0;
-            }
-            showPage(currentPage);
-        });
-
-        showPage(currentPage);
-
-        window.addEventListener('resize', () => {
-            showPage(currentPage);
-        });
+        // Initially display all food places
+        displayFoodPlaces(filteredFoodPlaces);
     })
     .catch(error => console.error('Error fetching data: ', error));
